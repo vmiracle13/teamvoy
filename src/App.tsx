54648d8cba95bf3bbb5fdd8c8a7d36b84a76b2fa
Viktoriya Borisovskaya <vmiracle13@gmail.com>
2023-08-04 11:37:18 +0300

    add reset css styles, change styles for headline

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
+import styled from 'styled-components/macro';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+const AppBox = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex: auto;
+`;
+
+const H1 = styled.h1`
+  margin: 1rem;
+  text-align: center;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <AppBox>
       <Header />
 
-      <main>
-        <h1>Pokedex</h1>
-      </main>
+      <Main>
+        <H1>Pokedex</H1>
+      </Main>
 
       <Footer />
-    </div>
+    </AppBox>
   );
 }
 
